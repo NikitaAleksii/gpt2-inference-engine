0461b76e56a886c57a6cfebf414e3fa671fc3ae6
Nikita Aleksii <nikitaaleksij@gmail.com>
2026-08-12 22:37:04 -0400

    changed tensor to be adjustable to any number of dimensions

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include "tensor.hpp"
 
-// Base constructor
-Tensor::Tensor() {}
-
 // Creates a tensor with newly allocated memory used for activations
 Tensor::Tensor(std::vector<int>& shape) {
-    // Stride is equal to the number of columns in a newly allocated tensor
-    this->stride = shape[1];
+    // Save shape of the tensor
+    this->shape  = shape;
+
+    compute_strides();
 
     // Allocate memory for a new tensor
-    this->buffer      = new float[shape[0] * shape[1]];
+    this->buffer      = new float[compute_size()];
     this->owns_buffer = true;
-
-    // Save shape of the tensor
-    this->shape  = move(shape);
 }
 
 // Creates a tensor with pre-assigned memory used for weights
 Tensor::Tensor(std::vector<int>& shape, void* data, size_t byte_offset) {
-    // Stride is equal to the number of columns in a pre-allocated data array
-    this->stride = shape[1];
+    // Save shape of the tensor
+    this->shape  = shape;
+
+    compute_strides();
 
     // Point to pre-allocated data
     char* base        = reinterpret_cast<char*>(data);
     this->buffer      = reinterpret_cast<float*>(base + byte_offset);
     this->owns_buffer = false;
-
-    // Save shape of the tensor
-    this->shape  = shape;
 }
